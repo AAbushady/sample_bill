72e748c09eb16422f278fbdbaa2661cc6e59315e
Alexander Abushady <aabushady214@gmail.com>
2019-09-08 16:13:58 -0400

    Update sample_bill.js
    Added Problem Statement

---
+++ sample_bill.js
@@ -0,0 +1,22 @@
+/*
+Spectrum internet has three different subscription packages for customers.
+
+Package A: For $19.95 per month 10 hours of access are provided. 
+Additional hours are $2.00 per hour.
+
+Package B: For $24.95 per month 20 hours of access are provided. 
+Additional hours are 1.00 per hour.
+
+Package C: For $29.95 per month unlimited access is provided.
+
+Customers have complained that it is difficult for them to determine what they will 
+have to pay when on the different plans. Corporate sales has decided to send 
+potential customers a sample bill outlaying what they would pay under each different plan.
+
+Write a program that will calculate and display a customer's monthly bill under
+each plan. Assume the customer uses an average of 52 hours per month. 
+For each of the plans show the amount of money they would pay under the plan.
+For package A show how much money they would save if they purchased package B or C. 
+For package B customers, show how much they would save if they purchased package C. 
+If there would be no savings, no message should be printed.
+*/
